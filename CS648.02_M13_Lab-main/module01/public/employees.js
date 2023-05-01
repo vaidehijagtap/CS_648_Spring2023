f08dd3f058d8925d13183b8cd6826e0945732a0f
vaidehijagtap <112371822+vaidehijagtap@users.noreply.github.com>
2023-04-30 21:39:07 -0700

    Module 13 Lab

---
+++ CS648.02_M13_Lab-main/module01/public/employees.js
@@ -0,0 +1,15 @@
+let anu = "Anu Jagtap";
+let prashant = "Prashant Ghadge";
+let seemant = "Seemant Gaikwad";
+let amol = "Amol Adhikari";
+let vaidehi = "Vaidehi Jagtap";
+let adwait = "Adwait Toro";
+let megha = "Megha Fadnis";
+let smita = "Smita Deshpande";
+const element = /*#__PURE__*/React.createElement("ul", {
+  style: {
+    color: "blue",
+    fontSize: "24px"
+  }
+}, /*#__PURE__*/React.createElement("li", null, anu), /*#__PURE__*/React.createElement("li", null, prashant), /*#__PURE__*/React.createElement("li", null, seemant), /*#__PURE__*/React.createElement("li", null, amol), /*#__PURE__*/React.createElement("li", null, vaidehi), /*#__PURE__*/React.createElement("li", null, adwait), /*#__PURE__*/React.createElement("li", null, megha), /*#__PURE__*/React.createElement("li", null, smita));
+ReactDOM.render(element, document.getElementById("content"));
---
+++ CS648.02_M13_Lab-main/module01/src/employees.jsx
@@ -0,0 +1,21 @@
+let anu = "Anu Jagtap";
+let prashant = "Prashant Ghadge";
+let seemant = "Seemant Gaikwad";
+let amol = "Amol Adhikari";
+let vaidehi = "Vaidehi Jagtap";
+let adwait = "Adwait Toro";
+let megha = "Megha Fadnis";
+let smita = "Smita Deshpande";
+const element = (
+  <ul style={{ color: "blue", fontSize: "24px" }}>
+    <li>{sally}</li>
+    <li>{mark}</li>
+    <li>{holly}</li>
+    <li>{amol}</li>
+    <li>{jay}</li>
+    <li>{saurabh}</li>
+    <li>{pankaj}</li>
+    <li>{eren}</li>
+  </ul>
+);
+ReactDOM.render(element, document.getElementById("content"));
